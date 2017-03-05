56373fd36b7c674d8154e0e24b66857485de33cc
laurisvan <lauri.svan@iki.fi>
2017-03-05 17:34:43 +0200

    Add some logging and error handling

--- hello-service/handler.js
+++ hello-service/handler.js
@@ -3,12 +3,48 @@
 module.exports.hello = (event, context, callback) => {
   console.info(`[Hello] Invoking Lambda with input ${JSON.stringify(event)}`);
 
-  const name = event.name;
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello, ${name}!`,
-      input: event,
-    }),
-  };
+  try {
+    // Manual error handling
+    if (typeof event !== 'object') {
+      throw new TypeError(`Invalid input: Expected object, got ${JSON.stringify(event)}`);
+    } else if (typeof event.name !== 'string') {
+      throw new TypeError(`Invalid input: .name should be a string`);
+    }
+
+    // Handle response
+    const name = event.name;
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        response: `Hello, ${name}!`,
+        input: event,
+      }),
+    };
+
+    callback(null, response);
+  } catch(error) {
+    console.warn(`[Hello] Caught an error: ${error.message}`);
+    console.warn(`[Hello] Stack trace: ${error.stack}`);
+
+    // Error defaults
+    let statusCode = 500;
+    let userMessage = 'Internal server error. Please try again later';
+
+    if (error instanceof TypeError) {
+      userMessage = 'Invalid input';
+      statusCode = 400;
+    }
+
+    const response = {
+      statusCode: statusCode,
+      body: {
+        userMessage: userMessage,
+        message: error.message,
+        stackTrace: error.stack,
+        input: event,
+      }
+    };
+
+    callback(null, response);
+  }
 };
