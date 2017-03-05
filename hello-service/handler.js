68ffc77b859407506a9fc859351372fd9f6381c1
laurisvan <lauri.svan@iki.fi>
2017-03-05 17:18:05 +0200

    Digest input that is not in API Gateway Proxy format

--- hello-service/handler.js
+++ hello-service/handler.js
@@ -1,16 +1,14 @@
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
+  console.info(`[Hello] Invoking Lambda with input ${JSON.stringify(event)}`);
+
+  const name = event.name;
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: `Hello, ${name}!`,
       input: event,
     }),
   };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
